59a4d7d32fec69fe4f1c6774411fef531f24a29e
Dratwas <drapich.piotr@gmail.com>
2019-09-12 23:12:33 +0200

    remove unused files

--- packages/babel-plugin-hmi-transformer/src/__tests__/index.js
+++
@@ -1,9 +0,0 @@
-import path from 'path'
-import pluginTester from 'babel-plugin-tester'
-import plugin from '../index.ts'
-
-pluginTester({
-  plugin,
-  fixtures: path.join(__dirname, 'fixtures'),
-  babelOptions: {filename: __filename},
-})
