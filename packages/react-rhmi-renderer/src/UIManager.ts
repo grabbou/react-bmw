42b39ff1a06325d9559131cb2cb3641f21fb0862
grabbou <grabbou@gmail.com>
2019-09-12 10:56:10 +0200

    Save work

--- packages/react-rhmi-renderer/src/UIManager.ts
+++ packages/react-rhmi-renderer/src/UIManager.ts
@@ -3,3 +3,30 @@ class UIManager {
 }
 
 export default UIManager;
+
+// ReactRHMIRenderer.runApplication(App);
+
+
+// ---
+
+
+// inversify
+
+// container.bind ourselves to inject the components 
+// THIS IS IN typescript/di
+
+// 1. start()
+
+// onRhmiReady -> hmiPresneters - init() - 
+// commnent: potential improvemt of lazy init
+
+// Sheer Developing Pleasure
+
+/**
+ * import ReactRHMIRenderer from 'react-bmw-renderer';
+ * import React from 'react';
+ * 
+ * import MainScreen from './MainScreen';
+ * 
+ * ReactRHMIRenderer.render(MainScreen);
+ */
