f494be2c04e0ec619104936a6237a99d0c330ad8
Dominik Schneider <dschneider@synyx.de>
2019-09-12 15:50:07 +0200

    REVERTME

--- packages/react-bmw/src/index.ts
+++ packages/react-bmw/src/index.ts
@@ -4,10 +4,15 @@
  * Sheer development pleasure
  */
 
-export { default as default } from './Renderer';
+// export { default as default } from './Renderer';
 
 /**
  * Public components
  */
 export {default as Container} from './components/Container';
 export {default as Text} from './components/Text';
+
+/**
+ * OAP-App Entrypoint
+ */
+export {default as UIManager} from './native/UIManager';
