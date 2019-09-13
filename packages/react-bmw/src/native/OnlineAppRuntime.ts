60fc097a62ca5f8bfc3653d8ff598c67b1fc2991
grabbou <grabbou@gmail.com>
2019-09-13 11:13:00 +0200

    Prettier format

--- packages/react-bmw/src/native/OnlineAppRuntime.ts
+++ packages/react-bmw/src/native/OnlineAppRuntime.ts
@@ -1,31 +1,34 @@
-import IOAPInterface from './IOAPInterface';
+import IOAPInterface from "./IOAPInterface";
 
 // @ts-ignore
-import OnlineApp from 'oap-sdk/src/core/OnlineApp';
+import OnlineApp from "oap-sdk/src/core/OnlineApp";
 
 const onlineApp = new OnlineApp();
 const rhmiIsReady = new Promise(resolve => {
-  onlineApp.on('rhmiReady', async () => {
+  onlineApp.on("rhmiReady", async () => {
     resolve();
   });
 });
 
-onlineApp.on('entryPointExecute', async () => {
+onlineApp.on("entryPointExecute", async () => {
   onlineApp.rhmiApplication.openEntryState(
-    '89065040-ce62-11e9-b5b0-959cc45744a0'
+    "89065040-ce62-11e9-b5b0-959cc45744a0"
   );
 });
 
-onlineApp.on('initialized', async () => {
+onlineApp.on("initialized", async () => {
   await onlineApp.initialized();
 });
 
 onlineApp.start();
 
 class OnlineAppRuntime implements IOAPInterface {
-  attachListenerToButton: (id: number, cb: () => void) => void = async (id, cb) => {
+  attachListenerToButton: (id: number, cb: () => void) => void = async (
+    id,
+    cb
+  ) => {
     const buttonHandle = onlineApp.rhmiApplication.getById(id);
-    buttonHandle.on('action', cb);
+    buttonHandle.on("action", cb);
   };
   openState: (id: number) => void = async id => {
     await rhmiIsReady;
@@ -35,14 +38,14 @@ class OnlineAppRuntime implements IOAPInterface {
     await rhmiIsReady;
     const entryState = onlineApp.rhmiApplication.getById(stateId);
     await onlineApp.rhmiApplication.mapEntryState(
-      '89065040-ce62-11e9-b5b0-959cc45744a0',
+      "89065040-ce62-11e9-b5b0-959cc45744a0",
       entryState
     );
 
     await entryState.updateResources();
-    if (onlineApp.startReason === 'ENTRY_POINT') {
+    if (onlineApp.startReason === "ENTRY_POINT") {
       await onlineApp.rhmiApplication.openEntryState(
-        '89065040-ce62-11e9-b5b0-959cc45744a0'
+        "89065040-ce62-11e9-b5b0-959cc45744a0"
       );
     }
   };
