aebef353d88cb3802306692d2955490e2bfb2ec7
Dominik Schneider <dschneider@synyx.de>
2019-09-13 10:15:54 +0200

    clean up leftovers

--- packages/react-bmw/src/native/OnlineAppRuntime.ts
+++ packages/react-bmw/src/native/OnlineAppRuntime.ts
@@ -1,5 +1,3 @@
-// @todo
-// to be implemented
 import IOAPInterface from './IOAPInterface';
 // @ts-ignore
 import OnlineApp from 'oap-sdk/src/core/OnlineApp';
@@ -45,39 +43,5 @@ class OnlineAppRuntime implements IOAPInterface {
     }
   };
 }
-
-// async start(entryPointId: string, stateId: number) {
-//   this.oap.on("entryPointExecute", async () => {
-//     this.oap.rhmiApplication.openEntryState(entryPointId);
-//   });
-
-//   this.oap.on("rhmiReady", async () => {
-//     const entryState = this.oap.rhmiApplication.getById(stateId);
-//     await entryState.updateResources();
-
-//     await this.oap.rhmiApplication.mapEntryState(entryPointId, entryState);
-
-//     if (this.oap.startReason === "ENTRY_POINT") {
-//       await this.oap.rhmiApplication.openEntryState(entryPointId);
-//     }
-//   });
-
-//   // @todo initialised event
-
-//   await this.oap.start();
-// }
-
-// async runApplication(
-//   ComponentFactory: new (ui: UIManager) => HMIState,
-//   entryPointId: string
-// ) {
-//   const ui = new UIManager(oap);
-
-//   const appInstance = new AppPresenter(ui);
-
-//   await appInstance.init();
-
-//   ui.start(entryPointId, 10);
-// }
 
 export default OnlineAppRuntime;
